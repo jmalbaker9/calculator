405cf54be9e60e0aa28b6ef77ba1150f4d0dacd0
jmalbaker9 <jmalbaker9@gmail.com>
2022-04-16 23:25:22 +0300

    created functions

--- calc.py
+++ calc.py
@@ -1,7 +1,13 @@
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+
 n1=int(input("Enter 1st number"))
 n2=int(input("Enter 2nd number"))
 
-sum_val=n1+n2
-diff=n1-n2
-print("sum=",sum_val)
-print("differnce=",diff)
+
+print("sum=",add(n1,n2))
+print("differnce=",subtract(n1,n2))
