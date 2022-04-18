44af6783135f68946baa9af96c50475df43b442a
Ashhadul Islam <ashhadulislam@Samirs-MacBook-Pro.local>
2022-04-19 00:01:43 +0300

    added percentage

--- calc.py
+++ calc.py
@@ -12,15 +12,21 @@ def subtract(n1,n2):
     '''    
     return n1-n2
 
+def percentage(n1,n2):
+    '''
+    returns n2% of n1'''
+    return n1*n2/100
 
 n1=int(input("Enter 1st number"))
 #taking first number as input
 n2=int(input("Enter 2nd number"))
 # taking second number as input
 
-oper=input("Choose operation - 1. add 2. sub")
+oper=input("Choose operation - 1. add 2. sub 3. perc")
 
 if oper=="add":
     print("sum=",add(n1,n2))
 elif oper=="sub":
     print("differnce=",subtract(n1,n2))
+elif oper=="perc":
+    print("percentage = ",percentage(n1,n2))
